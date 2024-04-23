38b715334d740d6e7c311518b0653bca65a6282d
sakula7 <149995271+sakula7@users.noreply.github.com>
2024-04-23 14:35:23 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://akulasanjayeswar:SANJAYMAHESH@cluster0.mwchpyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
